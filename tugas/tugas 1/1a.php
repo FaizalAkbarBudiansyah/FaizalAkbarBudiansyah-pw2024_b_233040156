038a74458c732c989c1904e8e0b170a3b5a34c20
FaizalAkbarBudiansyah <147482957+FaizalAkbarBudiansyah@users.noreply.github.com>
2024-02-20 08:30:39 +0700

    tugas pertama

---
+++ tugas/tugas 1/1a.php
@@ -0,0 +1,20 @@
+<?php
+$v1 = "Topi";
+$v2 = "Bundar";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>
+        <i>
+        <?php echo '"'. $v1 . " " . "saya" . " " . $v2 . " " . $v1 . "saya." . '"' ?>
+        </i>
+    </h2>
+</body>
+</html>
